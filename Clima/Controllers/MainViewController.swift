f2d3d558ca6519804d3db25f91a20a4706a2595b
1PALADIN1 <sonikx94@yandex.ru>
2022-04-20 22:30:57 +0300

    Подключил ссылки на объекты с MainStoryboard

--- Clima/Controllers/MainViewController.swift
+++ Clima/Controllers/MainViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var tempertureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
     
-
+    @IBAction func getCurrentLocationPressed(_ sender: UIButton) {
+    }
+    
 }
