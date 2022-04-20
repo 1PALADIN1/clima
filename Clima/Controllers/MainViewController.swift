077470ca3bb16c222b9a90896aa66407d62867d2
1PALADIN1 <sonikx94@yandex.ru>
2022-04-20 23:13:46 +0300

    Подключил модель к View

--- Clima/Controllers/MainViewController.swift
+++ Clima/Controllers/MainViewController.swift
@@ -10,22 +10,70 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var tempertureLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
+    
+    private var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let w = WeatherManager()
-        w.fetchWeather(for: "Moscow")
+        searchTextField.delegate = self
+        weatherManager.delegate = self
     }
     
-    @IBAction func searchPressed(_ sender: UIButton) {
-    }
+    
     
     @IBAction func getCurrentLocationPressed(_ sender: UIButton) {
     }
     
+}
+
+//MARK: - UITextFieldDelegate
+
+extension MainViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        
+        textField.placeholder = "Type something"
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = textField.text {
+            weatherManager.fetchWeather(for: city)
+        }
+        
+        textField.placeholder = "Search"
+        searchTextField.text = nil
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension MainViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
 }
